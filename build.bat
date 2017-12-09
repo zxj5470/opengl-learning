0910f515a411ef29c72220f6dc8c211fcedacfe8
zxj5470 <zxj5470@foxmail.com>
2017-12-10 02:16:09 +0800

    Initial commit

---
+++ build.bat
@@ -0,0 +1,2 @@
+"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe" "E:\VS2017 Workspace\opengltest\opengltest\opengltest.vcxproj"
+"%CD%\opengltest\Debug\opengltest.exe"
---
+++ setup.cpp
@@ -0,0 +1,8 @@
+//
+// Created by zh on 2017/12/10.
+//
+
+#include <iostream>
+int main(){
+    system("build.bat");
+}
